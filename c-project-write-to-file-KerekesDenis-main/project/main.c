1f59d543ee966b2a61762ec1060f8cc0e72f620b
KerekesDenis <denis.kerekes02@e-uvt.ro>
2025-05-29 23:38:48 +0300

    Add files via upload
    Correct file here

--- c-project-write-to-file-KerekesDenis-main/project/main.c
+++ c-project-write-to-file-KerekesDenis-main/project/main.c
@@ -3,14 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ORDERS 100
-
-// ANSI color codes
+// Color codes
 #define RED     "\x1b[31m"
 #define GREEN   "\x1b[32m"
 #define YELLOW  "\x1b[33m"
 #define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
 #define RESET   "\x1b[0m"
+
+#define MAX_ORDERS 100
 
 typedef struct {
     char restaurant[50];
@@ -21,8 +23,27 @@ typedef struct {
 Order orders[MAX_ORDERS];
 int orderCount = 0;
 
+void clearInputBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
+
+int getIntInput() {
+    int val;
+    while (true) {
+        int result = scanf("%d", &val);
+        if (result == 1) {
+            clearInputBuffer();
+            return val;
+        } else {
+            printf(RED "Invalid input. Please enter a number: " RESET);
+            clearInputBuffer();
+        }
+    }
+}
+
 void createCSV() {
-    char filePath[] = "C:\\Users\\bazinga\\Desktop\\project\\orders.csv";
+    char filePath[] = "C:\\Users\\bazinga\\Desktop\\c-project-write-to-file-KerekesDenis-main\\orders.txt";
     FILE *file = fopen(filePath, "a");
 
     if (file == NULL) {
@@ -45,18 +66,18 @@ void createCSV() {
 }
 
 void readCSV() {
-    char filePath[] = "C:\\Users\\bazinga\\Desktop\\project\\orders.csv";
+    char filePath[] = "C:\\Users\\bazinga\\Desktop\\c-project-write-to-file-KerekesDenis-main\\orders.txt";
     FILE *file = fopen(filePath, "r");
 
     if (file == NULL) {
-        printf(YELLOW "No previous orders found (file not created yet).\n" RESET);
+        printf(YELLOW "Error opening file for reading. If it's the first time, no previous orders.\n" RESET);
         return;
     }
 
     char buffer[200];
-    fgets(buffer, sizeof(buffer), file);
+    fgets(buffer, sizeof(buffer), file); // Skip header
 
-    while (fgets(buffer, sizeof(buffer), file)) {
+    while (fgets(buffer, sizeof(buffer), file) && orderCount < MAX_ORDERS) {
         Order newOrder;
         sscanf(buffer, "%49[^,],%49[^,],%99[^\n]", newOrder.restaurant, newOrder.item, newOrder.location);
         orders[orderCount++] = newOrder;
@@ -68,12 +89,12 @@ void readCSV() {
 
 void displayOrders() {
     if (orderCount == 0) {
-        printf(RED "\nNo previous orders found.\n" RESET);
+        printf("\n" YELLOW "No previous orders found.\n" RESET);
     } else {
-        printf(BLUE "\nPrevious Orders:\n" RESET);
+        printf("\n" CYAN "Previous Orders:\n" RESET);
         printf("Restaurant, Item, Location\n");
         for (int i = 0; i < orderCount; i++) {
-            printf(GREEN "%s" RESET ", " YELLOW "%s" RESET ", " BLUE "%s" RESET "\n",
+            printf(BLUE "%s" RESET ", " MAGENTA "%s" RESET ", " GREEN "%s" RESET "\n",
                    orders[i].restaurant, orders[i].item, orders[i].location);
         }
     }
@@ -87,26 +108,6 @@ void clearScreen() {
 #endif
 }
 
-int getValidatedIntInput(int min, int max, const char *prompt) {
-    int value;
-    while (1) {
-        printf("%s", prompt);
-        if (scanf("%d", &value) != 1) {
-            while (getchar() != '\n');
-            printf(RED "Invalid input. Please enter a number.\n" RESET);
-        } else if (value < min || value > max) {
-            printf(RED "Choice out of range (%d-%d). Try again.\n" RESET, min, max);
-        } else {
-            return value;
-        }
-    }
-}
-
-void getValidatedStringInput(char *buffer, int size, const char *prompt) {
-    printf("%s", prompt);
-    scanf("%99s", buffer);
-}
-
 int main() {
     char location[100];
     int choice = 0;
@@ -114,67 +115,162 @@ int main() {
 
     readCSV();
 
-    getValidatedStringInput(location, sizeof(location), GREEN "✅ Please enter your delivery location: " RESET);
+    printf("Please enter your location: ");
+    // Use fgets to allow spaces in location
+    if (fgets(location, sizeof(location), stdin) != NULL) {
+        // Remove newline at end if present
+        size_t len = strlen(location);
+        if (len > 0 && location[len - 1] == '\n') {
+            location[len - 1] = '\0';
+        }
+    }
 
     while (true) {
         clearScreen();
 
-        printf("\n" BLUE "Please select your restaurant:\n" RESET);
-        printf("1) KFC\n2) McDonald's\n3) Pizza Hut\n4) View Previous Orders\n5) Exit\n");
+        printf("\nPlease select your restaurant\n");
+        printf("1) " RED "KFC" RESET "\n");
+        printf("2) " YELLOW "McDonald's" RESET "\n");
+        printf("3) " GREEN "Pizza Hut" RESET "\n");
+        printf("4) View Previous Orders\n");
+        printf("5) Exit\n");
 
-        choice = getValidatedIntInput(1, 5, "Enter your choice (1-5): ");
+        printf("Enter your choice: ");
+        choice = getIntInput();
 
         switch (choice) {
             case 1:
-                printf(BLUE "\nKFC Menu:\n" RESET);
-                printf("1) Marti Bucket\n2) Hot Booster\n");
-                itemChoice = getValidatedIntInput(1, 2, "Enter your choice: ");
+                printf("\nYou selected " RED "KFC" RESET ", here's the menu:\n");
+                printf("1) " MAGENTA "Marti Bucket" RESET "\n");
+                printf("2) " MAGENTA "Hot Booster" RESET "\n");
+                printf("Enter your choice: ");
+                itemChoice = getIntInput();
 
-                snprintf(orders[orderCount].restaurant, sizeof(orders[orderCount].restaurant), "KFC");
-                snprintf(orders[orderCount].item, sizeof(orders[orderCount].item), itemChoice == 1 ? "Marti Bucket" : "Hot Booster");
-                snprintf(orders[orderCount].location, sizeof(orders[orderCount].location), "%s", location);
-                orderCount++;
+                if (itemChoice == 1 || itemChoice == 2) {
+                    if (orderCount >= MAX_ORDERS) {
+                        printf(RED "Order limit reached. Cannot add more orders.\n" RESET);
+                        break;
+                    }
 
-                printf(GREEN "\nYour order will be delivered to: %s\n" RESET, location);
+                    snprintf(orders[orderCount].restaurant, sizeof(orders[orderCount].restaurant), "KFC");
+                    snprintf(orders[orderCount].item, sizeof(orders[orderCount].item), itemChoice == 1 ? "Marti Bucket" : "Hot Booster");
+                    snprintf(orders[orderCount].location, sizeof(orders[orderCount].location), "%s", location);
+                    orderCount++;
+
+                    printf("You selected %s.\n", itemChoice == 1 ? "Marti Bucket" : "Hot Booster");
+                    printf("\nYour order will be delivered to your address: " CYAN "%s.\n" RESET, location);
+                } else {
+                    printf(RED "Invalid choice for KFC.\n" RESET);
+                }
                 break;
 
             case 2:
-                printf(BLUE "\nMcDonald's Menu:\n" RESET);
-                printf("1) Big Mac\n2) Fries\n3) Happy Meal\n");
-                itemChoice = getValidatedIntInput(1, 3, "Enter your choice: ");
+                printf("\nYou selected " YELLOW "McDonald's" RESET ", here's the menu:\n");
+                printf("1) " MAGENTA "Big Mac" RESET "\n");
+                printf("2) " MAGENTA "Fries" RESET "\n");
+                printf("3) " MAGENTA "Happy Meal" RESET "\n");
+                printf("Enter your choice: ");
+                itemChoice = getIntInput();
 
-                snprintf(orders[orderCount].restaurant, sizeof(orders[orderCount].restaurant), "McDonald's");
-                snprintf(orders[orderCount].item, sizeof(orders[orderCount].item),
-                         itemChoice == 1 ? "Big Mac" : itemChoice == 2 ? "Fries" : "Happy Meal");
-                snprintf(orders[orderCount].location, sizeof(orders[orderCount].location), "%s", location);
-                orderCount++;
+                if (itemChoice >= 1 && itemChoice <= 3) {
+                    if (orderCount >= MAX_ORDERS) {
+                        printf(RED "Order limit reached. Cannot add more orders.\n" RESET);
+                        break;
+                    }
 
-                printf(GREEN "\nYour order will be delivered to: %s\n" RESET, location);
+                    snprintf(orders[orderCount].restaurant, sizeof(orders[orderCount].restaurant), "McDonald's");
+
+                    if (itemChoice == 1)
+                        snprintf(orders[orderCount].item, sizeof(orders[orderCount].item), "Big Mac");
+                    else if (itemChoice == 2)
+                        snprintf(orders[orderCount].item, sizeof(orders[orderCount].item), "Fries");
+                    else
+                        snprintf(orders[orderCount].item, sizeof(orders[orderCount].item), "Happy Meal");
+
+                    snprintf(orders[orderCount].location, sizeof(orders[orderCount].location), "%s", location);
+                    orderCount++;
+
+                    printf("\nYour order will be delivered to your address: " CYAN "%s.\n" RESET, location);
+                } else {
+                    printf(RED "Invalid choice for McDonald's.\n" RESET);
+                }
                 break;
 
             case 3:
-                printf(BLUE "\nPizza Hut Menu:\n" RESET);
-                printf("1) Prosciutto Funghi pizza\n2) Custom Pizza\n");
-                itemChoice = getValidatedIntInput(1, 2, "Enter your choice: ");
+                printf("\nYou selected " GREEN "Pizza Hut" RESET ", here's the menu:\n");
+                printf("1) " MAGENTA "Prosciutto Funghi pizza" RESET "\n");
+                printf("2) " MAGENTA "Custom Pizza" RESET "\n");
+                printf("Enter your choice: ");
+                itemChoice = getIntInput();
 
                 if (itemChoice == 1) {
+                    if (orderCount >= MAX_ORDERS) {
+                        printf(RED "Order limit reached. Cannot add more orders.\n" RESET);
+                        break;
+                    }
+
                     snprintf(orders[orderCount].restaurant, sizeof(orders[orderCount].restaurant), "Pizza Hut");
                     snprintf(orders[orderCount].item, sizeof(orders[orderCount].item), "Prosciutto Funghi pizza");
+                    snprintf(orders[orderCount].location, sizeof(orders[orderCount].location), "%s", location);
+                    orderCount++;
+
+                    printf("\nYour order will be delivered to your address: " CYAN "%s.\n" RESET, location);
+                } else if (itemChoice == 2) {
+                    printf("\nPlease select the size:\n");
+                    printf("1) Large\n");
+                    printf("2) Small\n");
+                    printf("Enter your choice: ");
+                    int sizeChoice = getIntInput();
+
+                    if (sizeChoice == 1 || sizeChoice == 2) {
+                        printf("\nYou selected a %s pizza. Please select up to two ingredients:\n", sizeChoice == 1 ? "large" : "small");
+                        printf("1) Pepperoni\n");
+                        printf("2) Olives\n");
+                        printf("3) Mushrooms\n");
+
+                        int ingredientChoice1 = 0, ingredientChoice2 = 0;
+                        int ingredientCount = 0;
+
+                        printf("Enter your first ingredient choice: ");
+                        ingredientChoice1 = getIntInput();
+
+                        if (ingredientChoice1 >= 1 && ingredientChoice1 <= 3) {
+                            ingredientCount++;
+                        } else {
+                            printf(RED "Invalid first ingredient choice.\n" RESET);
+                        }
+
+                        if (ingredientCount < 2) {
+                            printf("Enter your second ingredient choice (or 0 to skip): ");
+                            ingredientChoice2 = getIntInput();
+
+                            if ((ingredientChoice2 >= 1 && ingredientChoice2 <= 3) && (ingredientChoice2 != ingredientChoice1)) {
+                                ingredientCount++;
+                            } else if (ingredientChoice2 == 0) {
+                                printf("No second ingredient selected.\n");
+                            } else {
+                                printf(RED "Invalid second ingredient choice.\n" RESET);
+                            }
+                        }
+
+                        if (orderCount >= MAX_ORDERS) {
+                            printf(RED "Order limit reached. Cannot add more orders.\n" RESET);
+                            break;
+                        }
+
+                        snprintf(orders[orderCount].restaurant, sizeof(orders[orderCount].restaurant), "Pizza Hut");
+                        snprintf(orders[orderCount].item, sizeof(orders[orderCount].item),
+                                 sizeChoice == 1 ? "Large Custom Pizza" : "Small Custom Pizza");
+                        snprintf(orders[orderCount].location, sizeof(orders[orderCount].location), "%s", location);
+                        orderCount++;
+
+                        printf("Your custom pizza will be delivered to your address: " CYAN "%s.\n" RESET, location);
+                    } else {
+                        printf(RED "Invalid size choice.\n" RESET);
+                    }
                 } else {
-                    int sizeChoice = getValidatedIntInput(1, 2, "Choose size: 1) Large 2) Small: ");
-                    printf("Choose up to 2 ingredients:\n1) Pepperoni\n2) Olives\n3) Mushrooms\n");
-
-                    int ing1 = getValidatedIntInput(1, 3, "First ingredient: ");
-                    int ing2 = getValidatedIntInput(0, 3, "Second ingredient (0 to skip): ");
-
-                    snprintf(orders[orderCount].restaurant, sizeof(orders[orderCount].restaurant), "Pizza Hut");
-                    snprintf(orders[orderCount].item, sizeof(orders[orderCount].item),
-                             sizeChoice == 1 ? "Large Custom Pizza" : "Small Custom Pizza");
+                    printf(RED "Invalid choice for Pizza Hut.\n" RESET);
                 }
-                snprintf(orders[orderCount].location, sizeof(orders[orderCount].location), "%s", location);
-                orderCount++;
-
-                printf(GREEN "\nYour order will be delivered to: %s\n" RESET, location);
                 break;
 
             case 4:
@@ -185,11 +281,20 @@ int main() {
                 printf(GREEN "Exiting the program.\n" RESET);
                 createCSV();
                 return 0;
+
+            default:
+                printf(RED "Invalid choice, please try again.\n" RESET);
+                break;
         }
 
-        int continueChoice = getValidatedIntInput(1, 2, "\nDo you want to make another order?\n1) Yes\n2) No\n");
+        printf("\nDo you want to make another order?\n");
+        printf("1) Yes\n");
+        printf("2) No\n");
+        printf("Enter your choice: ");
+        int continueChoice = getIntInput();
+
         if (continueChoice == 2) {
-            printf(GREEN "Thank you for your orders. Exiting.\n" RESET);
+            printf(GREEN "Thank you for your orders. Exiting the program.\n" RESET);
             createCSV();
             break;
         }
